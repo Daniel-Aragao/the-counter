44c8d975f7c374731b5a96887b4ab1ad4a3658b8
Daniel Aragão <danielfilhoce@gmail.com>
2022-01-05 23:11:16 -0500

    starting log service

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
+import { FirebaseApp } from 'firebase/app';
+import { User } from 'firebase/auth';
+import { Database, getDatabase, ref, query } from 'firebase/database';
+import { Observable } from 'rxjs';
+import { Log } from '../models/logs';
+import { getFireData, setFireData } from '../util/firestore-observable';
+import { v1 as guid } from 'uuid';
 
 @Injectable({
   providedIn: 'root',
@@ -10,16 +17,16 @@ export class LogService {
     this.db = getDatabase(app);
   }
 
-  getLog(id: string): Observable<User> {
+  getLogs(id: string): Observable<Log[]> {
     const reff = ref(this.db, 'user/' + id);
     const queryy = query(reff);
 
-    return getFireData<User>(queryy);
+    return getFireData<Log[]>(queryy);
   }
 
-  addUser(user: User): Observable<unknown> {
+  addUser(user: Log): Observable<unknown> {
     user.id = guid();
-    const reff = ref(this.db, 'user/' + user.id);
+    // const reff = ref(this.db, 'log/' + user.id);
 
     return setFireData(reff, user);
   }
